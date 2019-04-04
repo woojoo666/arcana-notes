34623046dd4b481ed93fa67af554f0e0fb823043
Max Woo <maxithewoowoo@gmail.com>
2019-04-03 21:09:14 -0700

    testing Nearley from "getting started" tutorial

---
+++ interpreter/nearley_test.js
@@ -0,0 +1,13 @@
+// from the "getting started" tutorial
+
+const nearley = require("nearley");
+const grammar = require("./nearley_test_grammar.js");
+
+// Create a Parser object from our grammar.
+const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
+
+// Parse something!
+parser.feed("foo\n");
+
+// parser.results is an array of possible parsings.
+console.log(parser.results); // [[[[ "foo" ],"\n" ]]]
