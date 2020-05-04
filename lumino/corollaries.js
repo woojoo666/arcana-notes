96e8ff972a6520a129c7a37887565f1d514818c1
Max Woo <maxithewoowoo@gmail.com>
2020-05-03 19:06:27 -0700

    using matchers and compose for shorthand transformer
    shorter but not necessarily cleaner...

--- lumino/corollaries.js
+++ lumino/corollaries.js
@@ -12,7 +12,7 @@
 
 import { UNDEFINED, Firefly } from './lumino';
 
-// note: each transformation is optional.
+// sends input template through a sequence of transformations.
 function transform (template) {
 	let pipeline = compose([
 		transformShorthand,
@@ -33,47 +33,40 @@ function compose(fns) {
 
 // see corollaries.test.js for an example of what shorthand looks like
 function transformShorthand (shorthand) {
+	const NEXT_REGEX = /NEXT/;
+	const VALUE_REGEX = /VALUE/;
 	const ACCESS_REGEX = /(\w+)\[(\w+)\]/;
 	const SPAWN_REGEX = /(\w+)\(\)/;
 	const INSERTION_REGEX = /(\w+)\s*\<\:\s*(\w+)/;
 
-	const parseProp = (prop) => {
-		if (typeof prop == 'object')
-			return prop; // we assume all object props are already transformed
+	const makeMatcher = function (regex, onMatch) {
+		return (str) => regex.test(str) ? onMatch(str.match(regex)) : str;
+	}
 
-		if (prop == 'NEXT')
-			return { type: 'inbox_next' };
-		if (prop == 'VALUE')
-			return { type: 'inbox_value' };
-	
-		if (ACCESS_REGEX.test(prop)) {
-			const [_, source, address] = prop.match(ACCESS_REGEX);
-			return { type: 'access', source, address };
-		}
-		if (SPAWN_REGEX.test(prop)) {
-			const [_, source] = prop.match(SPAWN_REGEX);
-			return { type: 'spawn', source };
-		}
-		throw Error(`unrecognized shorthand ${item}`);
+	// if input is an object, we assume it does not need any more transformations
+	const forwardObjects = function (transform) {
+		return (input) => typeof input == 'object' ? input : transform(input);
 	};
 
-	const parseItem = (item) => {
-		if (typeof item == 'object')
-			return item; // we assume all object items are already transformed
+	const parse = (prop) => {
+		const pipeline = [
+			makeMatcher(NEXT_REGEX, () => ({ type: 'inbox_next' })),
+			makeMatcher(VALUE_REGEX, () => ({ type: 'inbox_value' })),
+			makeMatcher(ACCESS_REGEX, ([_, source, address]) => ({ type: 'access', source, address })),
+			makeMatcher(SPAWN_REGEX, ([_, source]) => ({ type: 'spawn', source })),
+			makeMatcher(INSERTION_REGEX, ([_, target, source]) => ({ target, source })),
+			() => { throw Error(`unrecognized shorthand ${prop}`) },
+		].map(transform => forwardObjects(transform));
 
-		if (INSERTION_REGEX.test(item)) {
-			const [_, target, source] = item.match(INSERTION_REGEX);
-			return { target, source };
-		}
-		throw Error(`unrecognized shorthand ${item}`);
+		return compose(pipeline)(prop);
 	};
 
 	let template = { properties: {}, outbox: [] };
 	for (const [addr, prop] of Object.entries(shorthand.properties)) {
-		template.properties[addr] = parseProp(prop);
+		template.properties[addr] = parse(prop);
 	}
 	for (const item of shorthand.outbox) {
-		template.outbox.push(parseItem(item));
+		template.outbox.push(parse(item));
 	}
 	return template;
 }
